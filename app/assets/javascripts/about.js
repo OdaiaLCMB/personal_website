80c11d06af258a3e53fa94a892d9d6f501ecb277
odaialacombe <odaialcmb@gmail.com>
2017-11-22 16:12:25 +0100

    title about done

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,30 @@
+// const arrow = document.getElementById("arrow");
+// const card = document.querySelector('#card');
+
+// arrow.addEventListener("click", (event) => {
+//   const selected = card.querySelector('div.active');
+//   const next = card.querySelector('div.active + div');
+//   selected.classList.remove('hidden');
+//   next.classList.add('hidden');
+// });
+
+
+// // function unhide(clickedButton, divID) {
+// // var item = document.getElementById(divID);
+// // if (item) {
+// //     if(item.className=='hidden'){
+// //         item.className = 'unhidden' ;
+// //         clickedButton.value = 'hide'
+// //     }else{
+// //         item.className = 'hidden';
+// //         clickedButton.value = 'unhide'
+// //     }
+// // }}
+
+// // function unhide(divID, otherDivId) {
+// //     var item = document.getElementById(divID);
+// //     if (item) {
+// //             item.className=(item.className=='hidden')?'unhidden':'hidden';
+// //         }
+// //         document.getElementById(otherDivId).className = 'hidden';
+// //     }
