47911485e91ff2fe055b0587182d1c5ebe8f30be
odaialacombe <odaialcmb@gmail.com>
2017-11-23 13:47:13 +0100

    correction responsive et link footer choix entre langues corrigé

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  scope '(:locale)', locale: /en|fr/ do
+
+  scope '(:locale)', locale: /fr/ do
     root to: 'pages#home'
 
     get 'about', to: 'pages#about'
@@ -7,4 +8,9 @@ Rails.application.routes.draw do
     get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
+
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
 end
