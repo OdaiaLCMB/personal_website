b07429152251a32ff9187a242bd523d7eac8a40b
odaialacombe <odaialcmb@gmail.com>
2017-11-24 15:38:07 +0100

    adding jquery to about page

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,30 +1,19 @@
-// const arrow = document.getElementById("arrow");
-// const card = document.querySelector('#card');
+$('#arrow1').on('click', function(){
+  $( "#active" ).addClass( "hidden" );
+  $( "#active2" ).removeClass( "hidden" );
+});
 
-// arrow.addEventListener("click", (event) => {
-//   const selected = card.querySelector('div.active');
-//   const next = card.querySelector('div.active + div');
-//   selected.classList.remove('hidden');
-//   next.classList.add('hidden');
-// });
+$('#arrow2').on('click', function(){
+  $( "#active2" ).addClass( "hidden" );
+  $( "#active" ).removeClass( "hidden" );
+});
 
+$('#arrow3').on('click', function(){
+  $( "#active2" ).addClass( "hidden" );
+  $( "#active3" ).removeClass( "hidden" );
+});
 
-// // function unhide(clickedButton, divID) {
-// // var item = document.getElementById(divID);
-// // if (item) {
-// //     if(item.className=='hidden'){
-// //         item.className = 'unhidden' ;
-// //         clickedButton.value = 'hide'
-// //     }else{
-// //         item.className = 'hidden';
-// //         clickedButton.value = 'unhide'
-// //     }
-// // }}
-
-// // function unhide(divID, otherDivId) {
-// //     var item = document.getElementById(divID);
-// //     if (item) {
-// //             item.className=(item.className=='hidden')?'unhidden':'hidden';
-// //         }
-// //         document.getElementById(otherDivId).className = 'hidden';
-// //     }
+$('#arrow4').on('click', function(){
+  $( "#active3" ).addClass( "hidden" );
+  $( "#active2" ).removeClass( "hidden" );
+});
